50ab79ec7c719191bdeaa33d71e3eac610443996
Aloke Pramanik <129755834+hackerbotsupreme@users.noreply.github.com>
2023-11-14 20:05:27 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+#Introduction
+
+>Bookmarks is a versatile, high-performance landing page solution for marketing professionals. Built with HTML5 & CSS3,TailwindCss, a lot of thought and care went into this theme making it a pleasure to use.
+
+
+![alt text](https://github.com/radwan503/LandingPage--TailwindCss/blob/master/public/assets/image/ss.png)
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero': "url('./assets/image/bg.png')",
+       },
+      colors:{
+        "bookmark-purple":"#5267DF",
+        "bookmark-red":"#FA5959",
+        "bookmark-blue":"#242A45",
+        "bookmark-grey":"#9194A2",
+        "bookmark-white":"#f7f7f7"
+
+
+      }
+    },
+    fontFamily:{
+      Poppins: ['Poppins, sans-serif']
+    },
+    container:{
+      center:true,
+      padding:"1rem",
+      screens:{
+        lg:'1124px',
+        xl:'1124px',
+        '2xl':'1124px'
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
